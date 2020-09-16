8e8408a7e8daeb424a93a53b5d1393231ad58187
JasonRobit <suitjason@163.com>
2020-09-16 11:50:34 +0800

    添加简单列表

---
+++ app/src/main/java/com/baotu/demo/base/BaseFragment.java
@@ -0,0 +1,52 @@
+package com.baotu.demo.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * 作用基类：公共类
+ */
+public abstract class BaseFragment extends Fragment {
+
+    /**
+     * 上下文
+     */
+    protected Context mContext;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getActivity();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return initView();
+    }
+
+    /**
+     * 强制子类重写，实现子类特有ui
+     * @return
+     */
+    protected abstract View initView();
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    /**
+     *  当子类初始化数据，或者联网请求绑定数据，展示数据的，等等可以重写该方法
+     */
+    private void initData() {
+    }
+}
